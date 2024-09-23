8b80f2b91e98ebb3a38d4c140e3b340e7d7e81c2
Thibault Gleize <tibo.gleize@gmail.com>
2024-09-23 23:18:02 +0300

    Ajout du JS, version alpha fonctionnel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,98 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Variable globale permettant de gérer les bullets points
+// et l'indice du tableau slides
+let n = 0
+
+// function permettant de faire avancer le point avec gestion
+// de l'erreur de n 
+
+function NextDot(){
+	try {
+	let DotList = document.querySelectorAll(".dots .dot");
+	let ActiveDot = DotList[n]
+	let NextDot = DotList[n+1]
+	DotList[n].classList.remove("dot_selected")
+	DotList[n+1].classList.add("dot_selected")
+	ActiveDot = NextDot
+	n += 1
+	}
+	catch {
+	  let DotList = document.querySelectorAll(".dots .dot");
+	  n = 0
+	  DotList[n].classList.add("dot_selected")
+	}
+  }
+
+// function permettant de faire reculer le point avec gestion
+// de l'erreur de n 
+  
+  function PrevDot() {
+	try {
+	let DotList = document.querySelectorAll(".dots .dot");
+	let ActiveDot = DotList[n]
+	let PrevDot = DotList[n-1]
+	DotList[n].classList.remove("dot_selected")
+	DotList[n-1].classList.add("dot_selected")
+	ActiveDot = PrevDot
+	n -= 1
+	}
+	catch {
+	  let DotList = document.querySelectorAll(".dots .dot");
+	  n = 3
+	  DotList[n].classList.add("dot_selected")
+	}
+  }
+
+// Modification de banner-img en fonction de l'indice n dans 
+// tableau slides
+
+  function ChangeImgAndText (){
+	let img = document.querySelector(".banner-img")
+	let text = document.querySelector("#banner p")
+	let NewText = `
+	<p>${slides[n].tagLine}</p>
+	`;
+	img.src = "./assets/images/slideshow/"+ slides[n].image
+	text.innerHTML = NewText
+  }
+
+// Permet de calculer le nombre d'éléments dans tableau slides
+// et applique x nombres de divs avec class dot 
+// 1er indice contient dot_selected
+
+  function SlideNumber (){
+	let dots = document.querySelector(".dots")
+	let i = 0
+	while (i < slides.length) {
+		let DotDiv = document.createElement("div")
+		dots.appendChild(DotDiv)
+    if (i == 0) {
+      DotDiv.classList = "dot dot_selected"
+    } else {
+      DotDiv.classList = "dot"
+    }
+		i++
+	}
+}
+
+// function qui gère les événements et lance les fonctions
+// NextDot, PrevDot et ChangeImgAndText
+
+function SelectionFleche(){
+	let flecheGauche = document.querySelector(".arrow_left")
+	let flecheDroite = document.querySelector(".arrow_right")
+	  flecheDroite.addEventListener("click", () => {
+		NextDot()
+		ChangeImgAndText()
+	  });
+	   flecheGauche.addEventListener("click", () => {
+		 PrevDot()
+		 ChangeImgAndText()
+	  });
+  }
+
+SlideNumber()
+SelectionFleche()
